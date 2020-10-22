22c11ac46ab940056c611b1451a2f76eb45bbcb8
divitagulati <63459587+divitagulati@users.noreply.github.com>
2020-10-22 18:04:44 +0530

    Created armstrong_number.c
    Hey there,
    I have created a program to check whether a number is armstrong number or not.

---
+++ C Programs/armstrong_number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>  
+ int main()    
+{    
+int n,r,sum=0,temp;    
+printf("Enter the Number=");    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=sum+(r*r*r);    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("Armstrong Number ");    
+else    
+printf("Not Armstrong Number");    
+return 0;  
+}   
