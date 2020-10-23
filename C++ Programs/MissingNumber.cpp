56c1f4f50d1e196ffef420ae59aa7c3b06246233
Shehzad <55528726+ShehzadAhm@users.noreply.github.com>
2020-10-23 21:07:43 +0530

    Created MissingNumber.cpp

---
+++ C++ Programs/MissingNumber.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getMissingNo(int a[], int n) 
+{
+    int x1 = a[0]; 
+    int x2 = 1; 
+    for (int i = 1; i < n; i++) 
+        x1 = x1 ^ a[i]; 
+    for (int i = 2; i <= n + 1; i++) 
+        x2 = x2 ^ i; 
+    return (x1 ^ x2); 
+} 
+
+int main()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	int ans = getMissingNo(arr,n);
+	cout<<ans<<endl;
+	return 0;				
+}
