7b07e63e82795e5825abb36ce9c6596483c99ba0
Dylan Anderson <andedm12@uwgb.edu>
2020-11-20 11:11:55 -0600

    Cleaned Up Documentation describing QuickSort Function

--- C++ Programs/quicksort.cpp
+++ C++ Programs/quicksort.cpp
@@ -10,11 +10,11 @@ void swap(int* a, int* b)
 	*b = t; 
 } 
 
-/* This function takes last element as pivot, places 
+/* This function takes the last element as pivot, places 
 the pivot element at its correct position in sorted 
-array, and places all smaller (smaller than pivot) 
-to left of pivot and all greater elements to right 
-of pivot */
+array. It then places all smaller elements (smaller than pivot) 
+to the left of the pivot and all greater elements to the right 
+of the pivot */
 int partition (int arr[], int low, int high) 
 { 
 	int pivot = arr[high]; // pivot 
